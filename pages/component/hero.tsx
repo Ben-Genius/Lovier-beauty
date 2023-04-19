bf4272e41724f8d031fd9ae26d3cde7d1cccd575
azay <bernard.wiafe@afrilogicsolutions.com>
2023-04-19 23:26:44 +0000

    initial quote commit

--- pages/component/hero.tsx
+++ pages/component/hero.tsx
@@ -1,11 +1,87 @@
 import React from 'react'
+import { createStyles, Overlay, Container, Title, Button, Text, rem } from '@mantine/core';
+import hero1 from '../../public/hero1.jpeg'
+const useStyles = createStyles((theme) => ({
+  hero: {
+    position: 'relative',
+    backgroundImage:`url(${hero1})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
 
-type Props = {}
+  container: {
+    height: rem(700),
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+    paddingBottom: `calc(${theme.spacing.xl} * 6)`,
+    zIndex: 1,
+    position: 'relative',
 
-const hero = (props: Props) => {
+    [theme.fn.smallerThan('sm')]: {
+      height: rem(500),
+      paddingBottom: `calc(${theme.spacing.xl} * 3)`,
+    },
+  },
+
+  title: {
+    color: theme.white,
+    fontSize: rem(60),
+    fontWeight: 900,
+    lineHeight: 1.1,
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: rem(40),
+      lineHeight: 1.2,
+    },
+
+    [theme.fn.smallerThan('xs')]: {
+      fontSize: rem(28),
+      lineHeight: 1.3,
+    },
+  },
+
+  description: {
+    color: theme.white,
+    maxWidth: 600,
+
+    [theme.fn.smallerThan('sm')]: {
+      maxWidth: '100%',
+      fontSize: theme.fontSizes.sm,
+    },
+  },
+
+  control: {
+    marginTop: `calc(${theme.spacing.xl} * 1.5)`,
+
+    [theme.fn.smallerThan('sm')]: {
+      width: '100%',
+    },
+  },
+}));
+
+export default function Hero() {
+  const { classes } = useStyles();
+
   return (
-    <div>hero</div>
-  )
-}
+    <div className={classes.hero}>
+      <Overlay
+        gradient="linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, .65) 40%)"
+        opacity={1}
+        zIndex={0}
+      />
+      <Container className={classes.container}>
+        <Title className={classes.title}>WELCOME TO LOVIER BEAUTY HUB</Title>
+        <Text className={classes.description} size="xl" mt="xl">
+          Build fully functional accessible web applications faster than ever – Mantine includes
+          more than 120 customizable components and hooks to cover you in any situation
+        </Text>
 
-export default hero;
+        <Button variant="gradient" size="xl" radius="xl" className={classes.control}>
+          Get started
+        </Button>
+      </Container>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeaderMiddle from './component/HeaderMiddle';
 import links from './data'
+import Hero from './component/hero'
 const index = () => {
 
 
   return (
     <div>
       <HeaderMiddle links={links}/>
+      <Hero/>
     </div>
   )
 }
